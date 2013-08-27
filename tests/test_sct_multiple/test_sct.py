48528b315dde240fd3c4e2545233787f9329a982
Bryan Herman <bherman@mit.edu>
2013-08-27 14:56:08 -0400

    added another test for multiple sab tables but one has sct physics

---
+++ tests/test_sct_multiple/test_sct.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+from subprocess import Popen, STDOUT, PIPE
+import filecmp
+
+pwd = os.path.dirname(__file__)
+
+def setup(): 
+    os.putenv('PWD', pwd)
+    os.chdir(pwd)
+
+def test_run():
+    proc = Popen([pwd + '/../../src/openmc'], stderr=STDOUT, stdout=PIPE)
+    returncode = proc.wait()
+    print(proc.communicate()[0])
+    assert returncode == 0
+
+def test_created_statepoint():
+    assert os.path.exists(pwd + '/statepoint.10.binary')
+
+def test_results():
+    os.system('python results.py')
+    compare = filecmp.cmp('results_test.dat', 'results_true.dat')
+    if not compare:
+      os.rename('results_test.dat', 'results_error.dat')
+    assert compare
+
+def teardown():
+    output = [pwd + '/statepoint.10.binary', pwd + '/results_test.dat']
+    for f in output:
+        if os.path.exists(f):
+            os.remove(f)
